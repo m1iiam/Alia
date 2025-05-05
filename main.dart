8b7b39d9c11b40c16b46f4b42c64bd4a7a53b9a6
m1iiam <maiahmedsalem2021@gmail.com>
2025-05-05 13:01:02 -0700

    العداد

--- main.dart
+++ main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:untitled1/Home.dart';
-void main () {
+
+void main() {
   runApp(const MyApp());
 }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,11 +12,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'starter Template' ,
+      title: 'Counter App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: Home(),
+      home: const CounterScreen(),
+    );
+  }
+}
+
+//  StatefulWidget
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+
+  void _increaseCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decreaseCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App - Assignment'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          '$_counter',
+          style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: _increaseCounter,
+              tooltip: 'Add',
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              onPressed: _decreaseCounter,
+              tooltip: 'Subtract',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
